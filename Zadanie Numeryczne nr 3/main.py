507fab3415eec30aace6e3f0b3787410cbaa9c30
kulix <kulixon261@gmail.com>
2023-10-26 14:51:20 +0200

    add functions calculating L and U elements

--- Zadanie Numeryczne nr 3/main.py
+++ Zadanie Numeryczne nr 3/main.py
@@ -16,5 +16,20 @@ for i in range(N):
         mat[i][i+2] = (0.15)/(i+1)**2 # set 0.15/(i+1)**2 under diag
 
 
+def calcU(i, mat, matL, matU):
+    u = mat[i][i] - matL[i][i-1] * matU[i-1][i]
+    matU[i][i] = u
+
+def calcL1(i, mat, matL, matU):
+    l1 = (mat[i+1][i]) / matU[i][i]
+    matL[i+1][i] = l1
+
+def calcU1(i, mat, matL, matU):
+    u1 = mat[i][i+1] - matL[i][i+1]*matU[i-1][i+1]
+    matU[i+1][i] = u1
+
+def calcU2(i, mat, matL, matU):
+    u2 = mat[i][i+2]
+    matU[i+2][i] = u2
 
 print(np.matrix(mat))
