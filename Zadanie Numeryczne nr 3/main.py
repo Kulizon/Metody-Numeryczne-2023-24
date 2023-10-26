fix incorrect U1 formula

Zadanie Numeryczne nr 3/main.py
@@ -11,6 +11,34 @@ def initMatrix(N, mat):
         if (i+2 < N):
             mat[i][i+2] = (0.15)/(i+1)**2 # set 0.15/(i+1)**2 under diag
 
+def debug(N, arrU, arrU1, arrU2, arrL1):
+    # create L matrix
+    matL = [[0 for i in range(N)] for j in range(N)] 
+    for i in range(N):
+        matL[i][i] = 1
+        if (i+1 < N):
+            matL[i+1][i] = arrL1[i]
+
+    # create U matrix
+    matU = [[0 for i in range(N)] for j in range(N)] 
+    for i in range(N):
+        matU[i][i] = arrU[i]
+        if (i+1 < N):
+            matU[i][i+1] = arrU1[i]
+        if (i+2 < N):
+            matU[i][i+2] = arrU2[i]
+
+    # create original matrix by multiplying L and U
+    LU = np.matmul(np.matrix(matL), np.matrix(matU))
+
+    print("Matrix L")
+    print(np.matrix(matL))
+
+    print("Matrix U")
+    print(np.matrix(matU))
+
+    print()
+
 def calculateDiagDeterminal(mat):
     determinant = 1
     for i in range(len(mat)):
@@ -32,9 +60,14 @@ def solveAndMeasurePerformance(N):
     initMatrix(N, mat)
     start = time.perf_counter()
 
+    print("Og matrix")
+    print(np.matrix(mat))
+    print()
+
     for i in range(N):
+       
         # calc U
-        if (i-1 <= N):
+        if (i-1 >= 0):
             arrU[i] = mat[i][i]
         else:
             arrU[i] = mat[i][i] - arrL1[i-1] * arrU1[i]
@@ -44,12 +77,18 @@ def solveAndMeasurePerformance(N):
             arrL1[i] = (mat[i+1][i]) / arrU[i]
 
         # calc U1
-        if (i+1 < N and i-1 > -1):
-            arrU1[i] = mat[i][i+1] - arrL1[i]*arrU2[i]
+        if (i+1 < N):
+            if (i-1 >= 0):
+                arrU1[i] = mat[i][i+1] - arrL1[i]*arrU2[i-1]
+            else:
+                arrU1[i] = mat[i][i+1]
 
         # calc U2
         if (i+2 < N):
             arrU2[i] = mat[i][i+2]
+
+
+        debug(N, arrU, arrU1, arrU2, arrL1)
 
     end = time.perf_counter()
     delta = (end - start) * 1000
@@ -76,7 +115,9 @@ def solveAndMeasurePerformance(N):
     # check for equality
     #print(np.allclose(mat, LU, atol=0.1))
 
+    print("OG: ")
     print(np.matrix(mat))
+    print("LU: ")
     print(LU)
 
     # calculate determinant
@@ -88,8 +129,8 @@ def solveAndMeasurePerformance(N):
 # todo: sprawdz jak sie liczylo wyzacznik L, U skoro nie sa diagonalne
 # todo: solve equation x = (1, 2, ..., 124)^T etc
 
-
-solveAndMeasurePerformance(3)
+# todo: fix LU solver
+solveAndMeasurePerformance(5)
 
 """
 results = []