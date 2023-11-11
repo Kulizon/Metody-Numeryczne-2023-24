9c2af6188bb55835d6d0b40afad5fb8ca6fa19aa
kulix <kulixon261@gmail.com>
2023-11-11 23:21:12 +0100

    add task 4

--- Zadanie Numeryczne nr 4/main.py
+++ Zadanie Numeryczne nr 4/main.py
@@ -1,10 +1,8 @@
 import time
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 def backsubsitution(b, u, arrDiag, arrBandOverDiag1, N):
-    start = time.perf_counter()
-
     # Az = b
     z = [0 for i in range(N)]
     for i in range(N-1, -1, -1):
@@ -24,10 +22,9 @@ def backsubsitution(b, u, arrDiag, arrBandOverDiag1, N):
             
         y[i] = tmp / arrDiag[i]
 
-    end = time.perf_counter()
-    delta = (end - start) * 1000
 
-    return delta, z, y
+
+    return z, y
 
 def sumVec(vec, N):
     sum = 0
@@ -49,16 +46,17 @@ def vecSubVec(vec1, vec2, N):
 
 
 def solve(N, debug):
-    b = [5 for _ in range(N)]
+    start = time.perf_counter()
 
-    arrDiag = b = [11 for _ in range(N)]
+    b = [5 for _ in range(N)]
+    arrDiag = [11 for _ in range(N)]
     arrBandOverDiag1 = [7 for _ in range(N)]
 
     u = [1 for _ in range(N)]
-    #vt = [1 for _ in range(N)] # transponownae i nieistotne, bo mnozac przez to jedyne co robimy to dodajemy wszystkie elementy wektora
+    #vt = [1 for _ in range(N)] -> nieistotne, bo mnozac przez to jedyne co robimy to dodajemy wszystkie elementy wektora (bo elementy v^t to jedynki)
 
     # backsubstitution for Az = b and Ay = u
-    delta, z, y = backsubsitution(b, u, arrDiag, arrBandOverDiag1, N)
+    z, y = backsubsitution(b, u, arrDiag, arrBandOverDiag1, N)
 
     # x = z - [(v^t * z) * y] / [1 + v^t * y]  
     #          [liczba * wektor] / [liczba]
@@ -71,6 +69,9 @@ def solve(N, debug):
     result = scalarMulVec(gamma, y, N)
     x = vecSubVec(z, result, N)
 
+    end = time.perf_counter()
+    delta = (end - start) * 1000
+
     if (debug):
         mat = [[1 for j in range(N)] for i in range(N)]
 
@@ -81,18 +82,40 @@ def solve(N, debug):
         
         numpyResult = np.linalg.solve(mat, b)
 
-        print(arrDiag)
-        print(arrBandOverDiag1)
+        print("Is my result equal to numpy result?")
+        # print(numpyResult)
+        # print(x)
+        print(np.allclose(numpyResult, x, 0.00001))
 
-        print("Results: ")
-        print(numpyResult)
-        print(x)
+    return delta
 
-    
+solve(80, True)
 
+sum = 0
+numOfTests = 100
+for i in range(numOfTests):
+    sum += solve(80, False)
+print("Średni czas wykonania programu dla N = 80: ")
+print(sum / numOfTests)    
+print()
 
+results = []
+start = 100
+end = 10000
+step = 100
 
-solve(4, True)
-    
+for i in range(start, end, step):
+    runtime = solve(i, False)
+
+    results.append(runtime)
+    print("Czas wykonania dla N=" + str(i) + ": " + str(runtime))
+
+yPoints = [start + i * step for i in range(1, int(end / step))]
+
+plt.plot(yPoints, results, marker='o', linestyle='-')
+plt.xlabel('Parametr N')
+plt.ylabel('Czas działania funkcji (ms)')
+plt.title('Wykres zależności czasu wykonywania programu od parametru N')
+plt.show()
 
 
