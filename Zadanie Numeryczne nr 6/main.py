ae9309a56b9e4bac5c808670d4c0ca09b2dc08d2
kulix <kulixon261@gmail.com>
2023-11-21 22:25:43 +0100

    add power method

--- Zadanie Numeryczne nr 6/main.py
+++ Zadanie Numeryczne nr 6/main.py
@@ -3,30 +3,68 @@ import matplotlib.pyplot as plt
 import math
 import time
 
-def vectorNorm(A):
+def vecNorm(A):
     sum = 0
     for i in range(len(A)):
         sum += A[i]*A[i]
     return math.sqrt(sum)
 
-def scalarMulVec(scalar, A):
-    copy = A.copy()
-    for i in range(len(A)):
-        copy[i] = copy[i] * scalar
-    return copy
+def normalize(vec):
+    copy = vec.copy()
+    maxEl = max(vec)
+    for i in range(len(vec)):
+        copy[i] = copy[i] / maxEl
+    return maxEl, copy
 
+def matMulVec(mat, vec):
+    result = []
+    for i in range(len(mat)):
+        element = 0
+        for j in range(len(vec)):
+            element += mat[i][j] * vec[j]
 
-def eigenvaluePowerMethod(A):
-    startVec = [1 for _ in range(len(A))]
+        result.append(element)
+    return result
 
+def eigenvaluePowerMethod():
+    # arrUnderDiag = [0, 1, 2, 3]
+    # arrDiag = [8, 7, 6, 5]
+    # arrOverDiag = [1, 2, 3, 0]
+
+    A = [[4, 1, 0],
+         [0, 2, 1],
+         [0, 0, -1]]
+
+    curVec = [1 for _ in range(len(A))]
+    result = [curVec]
+
+    i = 0
     while(True):
-        norm = vectorNorm(startVec)
-        normedVec = scalarMulVec(norm, startVec)
+    
+        
+        curVec = matMulVec(A, curVec)
+        lamb, normedVec = normalize(curVec)
+        curVec = normedVec
 
+        print(lamb, curVec, normedVec)
+
+        result.append(normedVec)
+        
         # pomnóż A * normedVec i rozwiaz uklad A * normedVec = x_i
         # x_i unormuj i to bedzie normedVec w nastepnej iteracji
         # wykonuj dopóki y_m - y_m-1 < epsilon
 
+        # jednak pewnie bedzie wolno zbiegac wiec nalezy przesunac A - p * 1
+
+        diff = abs(vecNorm(result[len(result) - 1]) - vecNorm(result[len(result) - 2]))
+        i += 1
+        if (diff < 0.0001 or i > 10):
+            break
+
+    print(result[len(result) - 1])
+
+
+eigenvaluePowerMethod()
 
 def solve():
     return 1
