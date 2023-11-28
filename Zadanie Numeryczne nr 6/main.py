68f5ce1ae7989dc579b90d116971d239db5493b9
kulix <kulixon261@gmail.com>
2023-11-28 23:04:40 +0100

    add Wilkinson shift

--- Zadanie Numeryczne nr 6/main.py
+++ Zadanie Numeryczne nr 6/main.py
@@ -16,7 +16,7 @@ def normalize(vec):
         copy[i] = copy[i] / maxEl
     return maxEl, copy
 
-def matMulVec(mat, vec): # TODO: zrob to w O(1) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+def matMulVec(mat, vec): 
     result = []
     for i in range(len(mat)):
         element = 0
@@ -31,7 +31,11 @@ def maxEigenValueNumpy(A):
     maxEigenValue = np.max(np.abs(eigenValues)) 
     return maxEigenValue
 
-def eigenValuePowerMethod(A):
+def eigenValuePowerMethod(initialA, e, shiftVal = None):
+    A = initialA
+    if (shiftVal): # przesuniecie Wilkinsonskie
+        A = [[A[i][j] - shiftVal * (i == j) for j in range(len(A[0]))] for i in range(len(A))]
+
     curVec = [1 for _ in range(len(A))]
     resultVecs = [curVec]
     resultLambdas = [1]
@@ -46,8 +50,8 @@ def eigenValuePowerMethod(A):
 
         # jednak pewnie bedzie wolno zbiegac wiec nalezy przesunac A - p * 1
 
-        diff = abs(vecNorm(resultVecs[len(resultVecs) - 1]) - vecNorm(resultVecs[len(resultVecs) - 2]))
-        if (diff < 0.00000001):
+        diff = abs(resultLambdas[-1] - resultLambdas[-2])
+        if (diff < e / 100):
             break
 
     finalEigenVec = resultVecs[len(resultVecs) - 1]
@@ -55,14 +59,14 @@ def eigenValuePowerMethod(A):
     
     numpyEigenValues, numpyEigenVectors = np.linalg.eig(A)
     print("Czy moja maksymalna wartość własna obliczona metodą potęgową zgadza się z numpy?")
-    print(abs(max(numpyEigenValues) - finalEigenVal) < 0.000001)
+    print(abs(max(numpyEigenValues) - finalEigenVal) < e)
     print()
 
     finalNumpyMaxEigenValVec = []
     for i in range(4):
         finalNumpyMaxEigenValVec.append(numpyEigenVectors[i][0] / (-0.62856775)) # convert to my base
     print("Czy mój wektor odpowiadający największej wartości własnej zgadza się z numpy?")
-    print(np.allclose(finalEigenVec, finalNumpyMaxEigenValVec, 0.000001))
+    print(np.allclose(finalEigenVec, finalNumpyMaxEigenValVec, e))
     print()
 
     print(finalEigenVec)
@@ -78,16 +82,16 @@ def getDiagElements(A):
 def absSumUnderDiag(A):
     return abs(A[1][0]) + abs(A[2][0]) + abs(A[3][0]) + abs(A[1][2]) + abs(A[1][3]) + abs(A[2][3])
 
-def eigenValueQrMethod(A):
+def eigenValueQrMethod(A, e):
     curMat = A.copy()
     resultMats = [curMat]
     resultLambdas = [getDiagElements(curMat)]
 
     underDiagSums = [absSumUnderDiag(curMat)]
-    while(abs(underDiagSums[len(underDiagSums)-1]) > 0.000001):
+    while(abs(underDiagSums[-1]) > e):
         Q, R = np.linalg.qr(curMat)
 
-        nextMat = np.matmul(R, Q) # TODO: zrob w O(1) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        nextMat = np.matmul(R, Q)
         curMat = nextMat.copy()
         resultMats.append(curMat)
         resultLambdas.append(getDiagElements(curMat))
@@ -95,29 +99,60 @@ def eigenValueQrMethod(A):
         underDiagSums.append(absSumUnderDiag(curMat))
         # jak bedzie podobna do macierzy trojkatnej gornej to stop, wektory wlasne beda wtedy na diagonali
     
-    finalEigenVals = resultLambdas[len(resultLambdas)-1]
+    finalEigenVals = resultLambdas[-1]
 
     numpyEigenValues, numpyEigenVectors = np.linalg.eig(A)
     print("Czy moje wartości własne obliczone metodą rozkładu QR zgadzają się z numpy?")
-    print(np.allclose(finalEigenVals, numpyEigenValues, 0.000001))
+    print(np.allclose(finalEigenVals, numpyEigenValues, e))
     print()
 
     return resultMats, resultLambdas, underDiagSums
 
-def createPowerMethodPlot(eigenVals, A):
+def createPowerMethodPlot(eigenVals, initialA, shiftVal = None):
     xPointsPowerMethod = []
     yPointsPowerMethod = []
+
+    A = initialA
+    if (shiftVal):
+        A = [[A[i][j] - shiftVal * (i == j) for j in range(len(A[0]))] for i in range(len(A))]
+
     for i in range(len(eigenVals)):
         xPointsPowerMethod.append(i+1)
         yPointsPowerMethod.append(abs(eigenVals[i] - maxEigenValueNumpy(A)))
 
+    plt.figure(figsize=(7, 6))
     plt.plot(xPointsPowerMethod, yPointsPowerMethod, label="Metoda Potęgowa")
-
     plt.yscale("log")
     plt.legend()
     plt.xlabel('Numer iteracji')
-    plt.ylabel('Różnicy przybliżenia i dokładnej maksymalnej wartości własnej')
-    plt.title("Różnica przybliżenia i dokładnej maksymalnej wartości własnej \nzależnie od numeru iteracji dla metody potęgowej")
+    plt.ylabel('Różnica przybliżenia i dokładnej maksymalnej wartości własnej')
+    plt.title("Różnica przybliżenia i dokładnej maksymalnej wartości własnej \nzależnie od numeru iteracji dla metody potęgowej" + ("" if shiftVal == None else "\n oraz Wilkinsońskiego przesunięcia równego " + str(shiftVal)))
+    plt.show()
+
+def createPowerMethodComparisonPlot(eigenVals, eigenValsShifted, A, shiftVal):
+    xPointsPowerMethod = []
+    yPointsPowerMethod = []
+    xPointsPowerMethodShifted = []
+    yPointsPowerMethodShifted = []
+
+    shiftedA = [[A[i][j] - shiftVal * (i == j) for j in range(len(A[0]))] for i in range(len(A))]
+
+    for i in range(len(eigenVals)):
+        xPointsPowerMethod.append(i+1)
+        yPointsPowerMethod.append(abs(eigenVals[i] - maxEigenValueNumpy(A)))
+
+    for i in range(len(eigenValsShifted)):
+        xPointsPowerMethodShifted.append(i+1)
+        yPointsPowerMethodShifted.append(abs(eigenValsShifted[i] - maxEigenValueNumpy(shiftedA)))
+
+    plt.figure(figsize=(7, 6))
+    plt.plot(xPointsPowerMethod, yPointsPowerMethod, label="Metoda Potęgowa")
+    plt.plot(xPointsPowerMethodShifted, yPointsPowerMethodShifted, label="Metoda Potęgowa z Wilkinsońskim przesunięciem")
+    plt.yscale("log")
+    plt.legend()
+    plt.xlabel('Numer iteracji')
+    plt.ylabel('Różnica przybliżenia i dokładnej maksymalnej wartości własnej')
+    plt.title("Porównanie metody potęgowej bez przesunięcia \noraz z Wilkinsońskim przesunięciem równym " + str(shiftVal))
     plt.show()
 
 def createQrMethodPlot(eigenVals):
@@ -151,9 +186,10 @@ def createQrMethodTriangMatrixPlot(underDiagSums):
     xPoints = [i for i in range(len(underDiagSums))]
     yPoints = underDiagSums
 
+    plt.yscale("log")
     plt.plot(xPoints, yPoints)
     plt.xlabel('Numer iteracji')
-    plt.ylabel('Suma wartości bezwzględnych \nelementów pod diagonalą')
+    plt.ylabel('Logarytm z suma wartości bezwzględnych \nelementów pod diagonalą')
     plt.title('Suma wartości bezwzględnych elementów pod diagonalą \nzależnie od numeru iteracji')
     plt.show()
 
@@ -162,12 +198,16 @@ A = [[8, 1, 0, 0],
      [1, 7, 2, 0],
      [0, 2, 6, 3],
      [0, 0, 3, 5]]
-powerMethodEigenVecs, powerMethodEigenVals = eigenValuePowerMethod(A)
-qrMetodMats, qrMethodEigenVals, underDiagSums = eigenValueQrMethod(A)
+e = 0.00000001
+wilkinsonShift = -5
+_, powerMethodEigenVals = eigenValuePowerMethod(A, e)
+_, powerMethodEigenValsShifted = eigenValuePowerMethod(A, e, wilkinsonShift)
+_, qrMethodEigenVals, underDiagSums = eigenValueQrMethod(A, e)
 
-createPowerMethodPlot(powerMethodEigenVals, A)
-createQrMethodPlot(qrMethodEigenVals)
-createQrMethodTriangMatrixPlot(underDiagSums)
+# createPowerMethodPlot(powerMethodEigenVals, A) 
+createPowerMethodComparisonPlot(powerMethodEigenVals, powerMethodEigenValsShifted, A, wilkinsonShift)
+# createQrMethodPlot(qrMethodEigenVals)
+#createQrMethodTriangMatrixPlot(underDiagSums)
 
 
 
